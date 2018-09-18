792583a8505b533777b265aa6cabb8dc8b06d1e5
abbiemurugi <abigaelmurugi@gmail.com>
2018-09-18 19:36:10 +0300

    Made changes in form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription:boolean;
-    constructor(public id:number,public description:string, public name:string, public author:string,public likes:number, public dislikes:number, public createDate:Date){
+    constructor(public id:number,public description:string, public name:string, public author:string,public likes:number, public dislikes:number){
         this.showDescription=false
     }
 }
