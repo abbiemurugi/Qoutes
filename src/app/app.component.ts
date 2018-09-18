ef0665043259a66540601ae4d965aac46152106e
abbiemurugi <abigaelmurugi@gmail.com>
2018-09-18 10:10:15 +0300

    wrote the description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,7 @@ import { Quote } from './quote'
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  quotes = [
-    new Quote(1,'If you try, you risk failure. If you don’t, you ensure it.','James','Anonymous',new Date()),
-    new Quote(2, 'If you do what you need, you’re surviving. If you do what you want, you’re living.','Wesley','Unknown',new Date()),
-    new Quote(3, 'Go confidently in the direction of your dreams. Live the life you have imagined.','Hannah','Henry David Thoreau',new Date()),
-    new Quote(4, 'I would rather die a meaningful death than to live a meaningless life.','Mwangi','Corazon Aquino',new Date()),
-    new Quote(5, 'Without food,man can live at most but a few weeks;components of social justice are meaningless.','Tony','Harry Dean',new Date()),
-  ];
+ 
   constructor(){
   }
 }
