61f6ffb90e7a66f2ae434bf4fceab9da48bcacbb
abbiemurugi <abigaelmurugi@gmail.com>
2018-09-18 19:56:40 +0300

    Make

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, 'If you try, you risk failure. If you don’t, you ensure it.','James','Anonymous',0,0,new Date()),
-    new Quote(2, 'If you do what you need, you’re surviving. If you do what you want, you’re living.','Wesley','Unknown',0,0,new Date(2018,6,9)),
-    new Quote(3, 'Go confidently in the direction of your dreams. Live the life you have imagined.','Hannah','Henry David Thoreau',0,0,new Date(2018,5,7)),
-    new Quote(4, 'I would rather die a meaningful death than to live a meaningless life.','Mwangi','Corazon Aquino',0,0,new Date(2018,3,2)),
-    new Quote(5, 'Without food,man can live at most but a few weeks;components of social justice are meaningless.','Tony','Harry Dean',0,0, new Date(2018,3,4)),
+    new Quote(1, 'If you try, you risk failure. If you don’t, you ensure it.','James','Anonymous',0,0),
+    new Quote(2, 'If you do what you need, you’re surviving. If you do what you want, you’re living.','Wesley','Unknown',0,0),
+    new Quote(3, 'Go confidently in the direction of your dreams. Live the life you have imagined.','Hannah','Henry David Thoreau',0,0),
+    new Quote(4, 'I would rather die a meaningful death than to live a meaningless life.','Mwangi','Corazon Aquino',0,0),
+    new Quote(5, 'Without food,man can live at most but a few weeks;components of social justice are meaningless.','Tony','Harry Dean',0,0,),
   ]
 
   toogleDetails(index){
