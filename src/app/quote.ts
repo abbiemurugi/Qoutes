13b7548f591af43ec8dc76c61f3661b8d625a435
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-15 04:13:07 +0300

    added date argument

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription:boolean;
-    constructor(public id:number,public description:string, public name:string, public author:string,public likes:number, public dislikes:number){
+    constructor(public id:number,public description:string, public name:string, public author:string,public likes:number, public dislikes:number, public completeDate:Date){
         this.showDescription=false
     }
 }
